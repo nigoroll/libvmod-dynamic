28869d5e8b1dd3d213c7cdf66f325ec1200eb151
Tollef Fog Heen <tfheen@varnish-software.com>
2013-07-30 14:46:55 +0200

    Update for varnish 4.0, including reworking the build system

--- src/vmod_example.c
+++ src/vmod_example.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 
 #include "vrt.h"
-#include "bin/varnishd/cache.h"
+#include "cache/cache.h"
 
 #include "vcc_if.h"
 
@@ -11,22 +11,22 @@ init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 	return (0);
 }
 
-const char *
-vmod_hello(struct sess *sp, const char *name)
+VCL_STRING
+vmod_hello(const struct vrt_ctx *ctx, VCL_STRING name)
 {
 	char *p;
 	unsigned u, v;
 
-	u = WS_Reserve(sp->wrk->ws, 0); /* Reserve some work space */
-	p = sp->wrk->ws->f;		/* Front of workspace area */
+	u = WS_Reserve(ctx->ws, 0); /* Reserve some work space */
+	p = ctx->ws->f;		/* Front of workspace area */
 	v = snprintf(p, u, "Hello, %s", name);
 	v++;
 	if (v > u) {
 		/* No space, reset and leave */
-		WS_Release(sp->wrk->ws, 0);
+		WS_Release(ctx->ws, 0);
 		return (NULL);
 	}
 	/* Update work space with what we've used */
-	WS_Release(sp->wrk->ws, v);
+	WS_Release(ctx->ws, v);
 	return (p);
 }
