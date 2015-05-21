d0aa7f2de45b9a6e8ba273fc3b384a7eecd8bab4
Nils Goroll <nils.goroll@uplex.de>
2015-05-21 18:58:30 +0200

    emit the right error also for older pkg-config versions which don't

--- autogen.sh
+++ autogen.sh
@@ -35,10 +35,17 @@ else
 	esac
 fi
 
-set -ex
-
 # check for varnishapi.m4 in custom paths
-dataroot=$(pkg-config --variable=datarootdir varnishapi)
+dataroot=$(pkg-config --variable=datarootdir varnishapi 2>/dev/null)
+if [ -z "$dataroot" ] ; then
+	cat >&2 <<'EOF'
+Package varnishapi was not found in the pkg-config search path.
+Perhaps you should add the directory containing `varnishapi.pc'
+to the PKG_CONFIG_PATH environment variable
+EOF
+	exit 1
+fi
+set -ex
 aclocal -I m4 -I ${dataroot}/aclocal
 $LIBTOOLIZE --copy --force
 autoheader
