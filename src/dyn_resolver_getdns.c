f7ce2f88e8ba453288f65b82454197e0d636bc1b
Ryan Steinmetz <zi@FreeBSD.org>
2019-08-22 13:28:39 -0400

    Add missing headers when building under FreeBSD

--- src/dyn_resolver_getdns.c
+++ src/dyn_resolver_getdns.c
@@ -33,6 +33,12 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
+#ifdef __FreeBSD__
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <resolv.h>
+#endif
+
 #include <cache/cache.h>
 #include <vsa.h>
 
