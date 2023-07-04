nigoroll/libvmod-dynamic

commit 75546cf550e5bffe75fccd241e77206f126fbff2

Refactor: use VSA from backend endpoint
Avoid storing the suckaddr twice

src/vmod_dynamic.c
@@ -43,6 +43,7 @@
 #include <string.h>
 
 #include <cache/cache.h>
+#include <cache/cache_backend.h>
 
 #include <vsb.h>
 #include <vcl.h>
@@ -257,7 +258,6 @@ dynamic_del(VRT_CTX, struct dynamic_ref *r)
 
 	free(b->vcl_name);
 	free(b->ip_addr);
-	VSA_free(&b->ip_suckaddr);
 	free(b);
 }
 
@@ -279,6 +279,43 @@ dynamic_ref(VRT_CTX, struct dynamic_domain *dom, struct dynamic_backend *b)
 	    b->refcount);
 }
 
+/* select endpoint address matching sa proto */
+static const struct suckaddr *
+vep_select(const struct vrt_endpoint *vep, const struct suckaddr *sa)
+{
+
+	CHECK_OBJ_NOTNULL(vep, VRT_ENDPOINT_MAGIC);
+	switch (VSA_Get_Proto(sa)) {
+	case AF_INET:
+		return(vep->ipv4);
+	case AF_INET6:
+		return(vep->ipv6);
+	default:
+		WRONG("unexpected family");
+	}
+}
+
+static int
+vep_compare(const struct vrt_endpoint *vep, const struct suckaddr *sa)
+{
+	const struct suckaddr *vepsa;
+
+	vepsa = vep_select(vep, sa);
+	if (vepsa == NULL)
+		return (-1);
+	return (VSA_Compare(vepsa, sa));
+}
+
+static int
+bedir_compare_ip(VCL_BACKEND d, const struct suckaddr *sa)
+{
+	struct backend *be;
+
+	CHECK_OBJ_NOTNULL(d, DIRECTOR_MAGIC);
+	CAST_OBJ_NOTNULL(be, d->priv, BACKEND_MAGIC);
+	return (vep_compare(be->endpoint, sa));
+}
+
 static unsigned
 dynamic_find(struct dynamic_domain *dom, const struct suckaddr *sa)
 {
@@ -296,8 +333,7 @@ dynamic_find(struct dynamic_domain *dom, const struct suckaddr *sa)
 			continue;
 
 		b = r->be;
-		CHECK_OBJ_NOTNULL(b->dir, DIRECTOR_MAGIC);
-		if (!VSA_Compare(b->ip_suckaddr, sa)) {
+		if (! bedir_compare_ip(b->dir, sa)) {
 			r->mark = dom->mark;
 			return (1);
 		}
@@ -308,8 +344,7 @@ dynamic_find(struct dynamic_domain *dom, const struct suckaddr *sa)
 
 	/* search the rest of the backends */
 	VTAILQ_FOREACH(b, &dom->obj->backends, list) {
-		CHECK_OBJ_NOTNULL(b->dir, DIRECTOR_MAGIC);
-		if (!VSA_Compare(b->ip_suckaddr, sa)) {
+		if (! bedir_compare_ip(b->dir, sa)) {
 			dynamic_ref(&ctx, dom, b);
 			return (1);
 		}
@@ -351,7 +386,6 @@ dynamic_add(VRT_CTX, struct dynamic_domain *dom, const struct res_info *info)
 	b = malloc(sizeof *b);
 	AN(b);
 	memset(b, 0, sizeof *b);
-	b->ip_suckaddr = VSA_Clone(info->sa);
 
 	b->ip_addr = strdup(addr);
 	AN(b->ip_addr);
@@ -391,12 +425,12 @@ dynamic_add(VRT_CTX, struct dynamic_domain *dom, const struct res_info *info)
 	assert(vrt.proxy_header <= 2);
 	INIT_OBJ(&ep, VRT_ENDPOINT_MAGIC);
 
-	switch (VSA_Get_Proto(b->ip_suckaddr)) {
+	switch (VSA_Get_Proto(info->sa)) {
 	case AF_INET:
-		ep.ipv4 = b->ip_suckaddr;
+		ep.ipv4 = info->sa;
 		break;
 	case AF_INET6:
-		ep.ipv6 = b->ip_suckaddr;
+		ep.ipv6 = info->sa;
 		break;
 	default:
 		WRONG("unexpected family");

src/vmod_dynamic.h
@@ -42,7 +42,6 @@ extern struct VSC_lck *lck_be;
 struct dynamic_backend {
 	VCL_BACKEND			dir;
 	VTAILQ_ENTRY(dynamic_backend)	list;
-	const struct suckaddr		*ip_suckaddr;
 	char				*ip_addr;
 	char				*vcl_name;
 	unsigned			refcount;

commit 5c30a2f06024cee9db410ff55b2ed9a124b70f6f

FIX backend reuse for via
This fixes a regression introduced with
75546cf550e5bffe75fccd241e77206f126fbff2:

With via, we can not compare the backends' endpoints, because they point
to the via backend and the actual information is in the proxy header.

I do not want to re-introduce a container struct for backends, so for
now we just dup the suckaddr in the reference.

src/vmod_dynamic.c
@@ -323,6 +323,8 @@ ref_del(VRT_CTX, struct dynamic_ref *r)
 
 	DBG(ctx, r->dom, "unref-backend %s", be->vcl_name);
 	VRT_Assign_Backend(&r->dir, NULL);
+	if (r->sa != NULL)
+		VSA_free(&r->sa);
 	FREE_OBJ(r);
 }
 
@@ -352,9 +354,11 @@ ref_clone(VRT_CTX, struct dynamic_domain *dom, const struct dynamic_ref *s)
 
 	r = ref_new(dom);
 	VRT_Assign_Backend(&r->dir, s->dir);
+	r->keep = dom->obj->keep;
+	if (s->sa)
+		r->sa = VSA_Clone(s->sa);
 
 	VTAILQ_INSERT_TAIL(&dom->refs, r, list);
-	r->keep = dom->obj->keep;
 
 	CAST_OBJ_NOTNULL(be, s->dir->priv, BACKEND_MAGIC);
 
@@ -405,7 +409,7 @@ ref_compare_ip(struct dynamic_ref *r, const struct suckaddr *sa)
 {
 
 	CHECK_OBJ_NOTNULL(r, DYNAMIC_REF_MAGIC);
-	return (bedir_compare_ip(r->dir, sa));
+	return (r->sa ? VSA_Compare(r->sa, sa) : bedir_compare_ip(r->dir, sa));
 }
 
 static int
@@ -494,6 +498,8 @@ dynamic_add(VRT_CTX, struct dynamic_domain *dom, const struct res_info *info)
 	/* VRT_new_backend comes with a reference */
 	r = ref_new(dom);
 	r->dir = VRT_new_backend(ctx, &vrt, dom->obj->via);
+	if (dom->obj->via != NULL)
+		r->sa = VSA_Clone(info->sa);
 	VTAILQ_INSERT_TAIL(&dom->refs, r, list);
 
 	DBG(ctx, dom, "new-backend %s", vrt.vcl_name);

src/vmod_dynamic.h
@@ -48,6 +48,8 @@ struct dynamic_ref {
 	VTAILQ_ENTRY(dynamic_ref)	list;
 	struct dynamic_domain		*dom;
 	VCL_BACKEND			dir;
+	// if via is used
+	const struct suckaddr		*sa;
 };
 
 enum dynamic_status_e {